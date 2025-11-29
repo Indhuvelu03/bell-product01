2c25e7af01185aa4e4ecae2f2cf26b196b1ec80b
Indhuvelu03 <ammuindhu631@gmail.com>
2025-11-29 20:20:14 +0530

    Update ProductDetails.jsx

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import productsData from '../data/products.json';
+
 
 const ProductDetails = () => {
   const { id } = useParams();
